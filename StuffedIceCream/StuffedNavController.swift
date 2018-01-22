9ee99ea6809353026c65d311c5a29f70b6ea40e6
eldooon <eldonchan91@gmail.com>
2018-01-22 16:01:56 -0500

    Added custom nav bar

---
+++ StuffedIceCream/StuffedNavController.swift
@@ -0,0 +1,38 @@
+//
+//  StuffedNavController.swift
+//  StuffedIceCream
+//
+//  Created by Eldon Chan on 1/22/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class StuffedNavController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
