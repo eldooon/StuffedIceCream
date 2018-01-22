eb4d357163f846880ba82aedfb577a87460b7093
eldooon <eldonchan91@gmail.com>
2018-01-22 15:56:40 -0500

    First commit

---
+++ StuffedIceCream/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  StuffedIceCream
+//
+//  Created by Eldon Chan on 1/22/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.]
+        
+        view.backgroundColor = .red
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
