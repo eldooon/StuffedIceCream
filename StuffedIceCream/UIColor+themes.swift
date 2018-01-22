6113417af7328d0abfbb9dc5cbcf54b02a4f3896
eldooon <eldonchan91@gmail.com>
2018-01-22 16:04:16 -0500

    Added color file

---
+++ StuffedIceCream/UIColor+themes.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+themes.swift
+//  StuffedIceCream
+//
+//  Created by Eldon Chan on 1/22/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static let stuffedBlue = UIColor(red: 140/255, green: 235/255, blue: 221/255, alpha: 1)
+    
+}
