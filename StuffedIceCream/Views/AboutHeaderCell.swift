42230bb68cb74a19367d7984ad70ba300e4cc8a5
eldooon <eldonchan91@gmail.com>
2018-01-24 15:19:01 -0500

    Added Divider line, changed some properties for better UI

--- StuffedIceCream/Views/AboutHeaderCell.swift
+++ StuffedIceCream/Views/AboutHeaderCell.swift
@@ -19,7 +19,14 @@ class AboutHeaderCell: UICollectionViewCell {
     
     let itemTextView: UITextView = {
         let tv = UITextView()
+        tv.isScrollEnabled = false
         return tv
+    }()
+    
+    let dividerLine: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.1)
+        return view
     }()
     
     override init(frame: CGRect) {
@@ -38,7 +45,10 @@ class AboutHeaderCell: UICollectionViewCell {
         itemImageView.anchor(centerX: nil, centerY: nil, top: topAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 20, paddingLeft: 20, paddingBottom: 20, paddingRight: 20, width: 0, height: 200)
         
         addSubview(itemTextView)
-        itemTextView.anchor(centerX: nil, centerY: nil, top: itemImageView.bottomAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 20, paddingLeft: 10, paddingBottom: 20, paddingRight: 10, width: 0, height: 0)
+        itemTextView.anchor(centerX: nil, centerY: nil, top: itemImageView.bottomAnchor, left: leftAnchor, bottom: nil  , right: rightAnchor, paddingTop: 5, paddingLeft: 10, paddingBottom: 0, paddingRight: 10, width: 0, height: 400)
+        
+        addSubview(dividerLine)
+        dividerLine.anchor(centerX: nil, centerY: nil, top: itemTextView.bottomAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 20, paddingLeft: 10, paddingBottom: 0, paddingRight: 10, width: 0, height: 1)
     }
     
 }
