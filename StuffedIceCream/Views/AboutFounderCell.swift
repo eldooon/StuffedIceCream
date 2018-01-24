419eefce5b34f6dcd3a0f0b4065e1680161a7d2a
eldooon <eldonchan91@gmail.com>
2018-01-24 15:48:36 -0500

    Added more left padding to imageview

--- StuffedIceCream/Views/AboutFounderCell.swift
+++ StuffedIceCream/Views/AboutFounderCell.swift
@@ -35,7 +35,7 @@ class AboutFounderCell: UICollectionViewCell {
     private func createLayout() {
         
         addSubview(founderImageView)
-        founderImageView.anchor(centerX: nil, centerY: nil, top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 5, paddingLeft: 5, paddingBottom: 0, paddingRight: 0, width: 100, height: 100)
+        founderImageView.anchor(centerX: nil, centerY: nil, top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 5, paddingLeft: 20, paddingBottom: 0, paddingRight: 0, width: 100, height: 100)
         
         addSubview(founderNameLabel)
         founderNameLabel.anchor(centerX: nil, centerY: nil, top: founderImageView.topAnchor, left: founderImageView.rightAnchor, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 20, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
