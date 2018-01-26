9d26707e25658687e2944d6b590333d3441e58de
eldooon <eldonchan91@gmail.com>
2018-01-26 16:23:58 -0500

    added ScrollView, Contentview, and single textfield to VC

--- StuffedIceCream/Controllers/CateringController.swift
+++ StuffedIceCream/Controllers/CateringController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class CateringController: UIViewController {
+    
+    var scrollView : UIScrollView = {
+        let sv = UIScrollView()
+        sv.sizeToFit()
+        return sv
+    }()
+    
+    let contentView : UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    let firstNameTextField: UITextField = {
+        let tf = UITextField()
+        tf.backgroundColor = .stuffedBlue
+        return tf
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        createLayout()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    private func createLayout() {
+        
+        view.addSubview(scrollView)
+        scrollView.anchor(centerX: nil, centerY: nil, top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
+        
+        scrollView.addSubview(contentView)
+        contentView.anchor(centerX: nil, centerY: nil, top: scrollView.topAnchor, left: nil, bottom: scrollView.bottomAnchor, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: self.view.bounds.width, height: scrollView.frame.height)
+        
+        contentView.addSubview(firstNameTextField)
+        firstNameTextField.anchor(centerX: nil, centerY: nil, top: contentView.topAnchor, left: contentView.leftAnchor, bottom: nil, right: contentView.rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 0, paddingRight: 10, width: 0, height: 0)
+    }
+
     
 
     /*
