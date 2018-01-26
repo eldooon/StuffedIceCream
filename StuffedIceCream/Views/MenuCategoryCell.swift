a35555c022b6daf8cf20e879dd350e3aee9b7697
eldooon <eldonchan91@gmail.com>
2018-01-26 12:13:11 -0500

    Added MenuCategoryCell

---
+++ StuffedIceCream/Views/MenuCategoryCell.swift
@@ -0,0 +1,22 @@
+//
+//  MenuCategoryCell.swift
+//  StuffedIceCream
+//
+//  Created by Eldon Chan on 1/26/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class MenuCategoryCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
