e8293ba24a11c38248d748740017b49e91d3597d
eldooon <eldonchan91@gmail.com>
2018-02-02 11:57:41 -0500

    Added Location VC

---
+++ StuffedIceCream/Controllers/LocationController.swift
@@ -0,0 +1,36 @@
+//
+//  LocationController.swift
+//  StuffedIceCream
+//
+//  Created by Eldon Chan on 2/2/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class LocationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
