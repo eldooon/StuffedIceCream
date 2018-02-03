0f22ca61955d20d8cbbed03367bf241a41623dec
eldooon <eldonchan91@gmail.com>
2018-02-03 14:31:46 -0500

    Added back sidemenuitem due to error code -1

---
+++ StuffedIceCream/Model/SideMenuItem.swift
@@ -0,0 +1,27 @@
+//
+//  SideMenuItem.swift
+//  StuffedIceCream
+//
+//  Created by Eldon Chan on 2/3/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+public class SideMenuItem {
+    
+    let menuName: String?
+    let menuImage: UIImage?
+    
+    init(Image: UIImage?, Name: String?){
+        
+        menuName = Name
+        menuImage  = Image
+    }
+    
+}
+
+class MenuItems {
+    
+    let items = [SideMenuItem(Image: #imageLiteral(resourceName: "aboutIcon"), Name: "About"), SideMenuItem(Image: #imageLiteral(resourceName: "jobIcon"), Name: "Location"), SideMenuItem(Image: #imageLiteral(resourceName: "menuIcon"), Name: "Menu"), SideMenuItem(Image: #imageLiteral(resourceName: "cateringIcon"), Name: "Catering"), SideMenuItem(Image: #imageLiteral(resourceName: "shopIcon"), Name: "Shop")]
+}
