395d32efd6b64a823f878284ece877b643baaabc
eldooon <eldonchan91@gmail.com>
2018-02-03 14:17:53 -0500

    Added MenuCategory model

---
+++ StuffedIceCream/Model/MenuCategory.swift
@@ -0,0 +1,15 @@
+//
+//  MenuCategory.swift
+//  StuffedIceCream
+//
+//  Created by Eldon Chan on 2/3/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class MenuCategory {
+    
+    var name = String()
+    
+}
