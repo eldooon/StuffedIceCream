fb0cafe0e19bb3fa6549174923606b138177135e
eldooon <eldonchan91@gmail.com>
2018-02-03 14:28:35 -0500

    reverting

---
+++ StuffedIceCream/Model/SideMenuItem.swift
@@ -0,0 +1,29 @@
+//
+//  File.swift
+//  StuffedIceCream
+//
+//  Created by Eldon Chan on 1/23/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+public class SideMenuItem {
+    
+    let menuName: String?
+    let menuImage: UIImage?
+    
+    init(Image: UIImage?, Name: String?){
+        
+        menuName = Name
+        menuImage  = Image
+    }
+    
+}
+
+class MenuItems {
+    
+    let items = [SideMenuItem(Image: #imageLiteral(resourceName: "aboutIcon"), Name: "About"), SideMenuItem(Image: #imageLiteral(resourceName: "jobIcon"), Name: "Location"), SideMenuItem(Image: #imageLiteral(resourceName: "menuIcon"), Name: "Menu"), SideMenuItem(Image: #imageLiteral(resourceName: "cateringIcon"), Name: "Catering"), SideMenuItem(Image: #imageLiteral(resourceName: "shopIcon"), Name: "Shop")]
+}
+
+
