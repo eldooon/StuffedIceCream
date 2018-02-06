3a9ae4b42ccd85cd3e2fd821e18ddcc0c0762701
eldooon <eldonchan91@gmail.com>
2018-02-06 14:12:37 -0500

    Added imageview and label

--- StuffedIceCream/Controllers/ItemDetailController.swift
+++ StuffedIceCream/Controllers/ItemDetailController.swift
@@ -10,10 +10,24 @@ import UIKit
 
 class ItemDetailController: UIViewController {
     
+    let itemImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        iv.backgroundColor = .blue
+        return iv
+    }()
+    
+    let itemNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Item Name"
+        label.font = UIFont.primary
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
         createLayout()
     }
     
@@ -24,5 +38,10 @@ class ItemDetailController: UIViewController {
     
     private func createLayout() {
         
+        view.addSubview(itemImageView)
+        itemImageView.anchor(centerX: nil, centerY: nil, top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 10, paddingLeft: 10, paddingBottom: 0, paddingRight: 10, width: 0, height: 200)
+        
+        view.addSubview(itemNameLabel)
+        itemNameLabel.anchor(centerX: view.centerXAnchor, centerY: nil, top: itemImageView.bottomAnchor, left: nil, bottom: nil, right: nil, paddingTop: 10, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0)
     }
 }
