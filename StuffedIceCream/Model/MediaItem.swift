77e81869bd8c144142545de13dae0ee2cc8760f6
eldooon <eldonchan91@gmail.com>
2018-02-12 14:29:46 -0500

    Added MediaItem

---
+++ StuffedIceCream/Model/MediaItem.swift
@@ -0,0 +1,21 @@
+//
+//  MediaItem.swift
+//  StuffedIceCream
+//
+//  Created by Eldon Chan on 2/12/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+public class MediaItem {
+    
+    let mediaImage: UIImage?
+    
+    init(Image: UIImage?){
+    
+        mediaImage  = Image
+    }
+    
+}
+
