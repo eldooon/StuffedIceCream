d14c27b74e21d62c00fe8671fd0c20a7dcf27569
eldooon <eldonchan91@gmail.com>
2018-02-12 14:23:53 -0500

    Added HomeItemCell properties

--- StuffedIceCream/Views/HomeItemCell.swift
+++ StuffedIceCream/Views/HomeItemCell.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2018 ByEldon. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class HomeItemCell: UICollectionViewCell {
+    
+    
+    let itemImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFit
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        createLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func createLayout(){
+        
+        addSubview(itemImageView)
+        itemImageView.anchor(centerX: nil, centerY: nil, top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 140)
+        
+    }
+    
+}
+
