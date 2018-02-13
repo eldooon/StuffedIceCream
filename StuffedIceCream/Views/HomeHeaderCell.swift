02a586df5f5aa1d19074b2ac4a5f3ae9bf3ee6f4
eldooon <eldonchan91@gmail.com>
2018-02-13 14:48:31 -0500

    Set cell headeritem properties

--- StuffedIceCream/Views/HomeHeaderCell.swift
+++ StuffedIceCream/Views/HomeHeaderCell.swift
@@ -87,6 +87,7 @@ class HomeHeaderCell: UICollectionViewCell, UICollectionViewDataSource, UICollec
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! HomeHeaderItemCell
+        cell.headerItem = headerImages?[indexPath.item]
         return cell
     }
     
