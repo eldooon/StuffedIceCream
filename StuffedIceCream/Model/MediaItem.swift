a175c8a64eb028f453807a162627688472b11c51
eldooon <eldonchan91@gmail.com>
2018-02-14 12:49:31 -0500

    Added link to MediaItem

--- StuffedIceCream/Model/MediaItem.swift
+++ StuffedIceCream/Model/MediaItem.swift
@@ -11,18 +11,20 @@ import UIKit
 public class MediaItem {
     
     let mediaImage: UIImage?
+    let mediaLink: String?
     
-    init(Image: UIImage?){
+    init(Image: UIImage?, Link: String?){
     
-        mediaImage  = Image
+        mediaImage = Image
+        mediaLink = Link
     }
     
 }
 
 class MediaItems {
     
-    let items = [MediaItem(Image: #imageLiteral(resourceName: "Insider")), MediaItem(Image: #imageLiteral(resourceName: "Thrillist")), MediaItem(Image: #imageLiteral(resourceName: "BuzzFeed")), MediaItem(Image: #imageLiteral(resourceName: "Eater")), MediaItem(Image: #imageLiteral(resourceName: "NBC")), MediaItem(Image:#imageLiteral(resourceName: "Fox5")), MediaItem(Image: #imageLiteral(resourceName: "Gothamist")), MediaItem(Image: #imageLiteral(resourceName: "TimeOutNY")), MediaItem(Image: #imageLiteral(resourceName: "Best")), MediaItem(Image: #imageLiteral(resourceName: "Cosmopolitan")), MediaItem(Image: #imageLiteral(resourceName: "FoodNetwork")), MediaItem(Image: #imageLiteral(resourceName: "DiningWithSkyler")), MediaItem(Image: #imageLiteral(resourceName: "Where")), MediaItem(Image: #imageLiteral(resourceName: "ExtraCrispy")), MediaItem(Image: #imageLiteral(resourceName: "Refinery29")), MediaItem(Image: #imageLiteral(resourceName: "ForkYeah")), MediaItem(Image: #imageLiteral(resourceName: "Metro")), MediaItem(Image: #imageLiteral(resourceName: "Bedford+Bowery"))]
+    let items = [MediaItem(Image: #imageLiteral(resourceName: "Insider"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "Thrillist"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "BuzzFeed"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "Eater"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "NBC"), Link: nil), MediaItem(Image:#imageLiteral(resourceName: "Fox5"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "Gothamist"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "TimeOutNY"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "Best"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "Cosmopolitan"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "FoodNetwork"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "DiningWithSkyler"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "Where"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "ExtraCrispy"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "Refinery29"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "ForkYeah"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "Metro"), Link: nil), MediaItem(Image: #imageLiteral(resourceName: "Bedford+Bowery"), Link: nil)]
     
-    let socialItems = [MediaItem(Image: #imageLiteral(resourceName: "Facebook")), MediaItem(Image: #imageLiteral(resourceName: "Instagram"))]
+    let socialItems = [MediaItem(Image: #imageLiteral(resourceName: "Facebook"), Link: "https://www.facebook.com/StuffedIceCreamNYC"), MediaItem(Image: #imageLiteral(resourceName: "Instagram"), Link: "https://www.instagram.com/stuffedicecreamnyc")]
 }
 
