b01680db56e1cd244e3455985008dbf7d97517ba
eldooon <eldonchan91@gmail.com>
2018-02-23 13:38:56 -0500

    refactor ot use imageconversion file

--- StuffedIceCream/Views/HomeHeaderItemCell.swift
+++ StuffedIceCream/Views/HomeHeaderItemCell.swift
@@ -22,21 +22,7 @@ class HomeHeaderItemCell: UICollectionViewCell {
             
             if let imageURL = headerItem?.image {
                 
-                guard let url = URL(string: imageURL) else { return }
-                
-                URLSession.shared.dataTask(with: url, completionHandler: { (data, response, err) in
-                    if let err = err {
-                        print("Failed to convert this image due to", err)
-                    }
-                    
-                    guard let imageData = data else { return }
-                    let image = UIImage(data: imageData)
-                    
-                    DispatchQueue.main.async {
-                        self.itemImageView.image = image
-                    }
-                    
-                }).resume()
+                ImageConversion.convertStringToImage(imageURL: imageURL, imageView: itemImageView)
             }
         }
     }
