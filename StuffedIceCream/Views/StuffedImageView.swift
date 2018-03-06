3792acdb1fe3dfabd50f8b4804821d9494a9228e
eldooon <eldonchan91@gmail.com>
2018-03-05 20:55:37 -0500

    added caching and conversion to image from string

--- StuffedIceCream/Views/StuffedImageView.swift
+++ StuffedIceCream/Views/StuffedImageView.swift
@@ -8,14 +8,45 @@
 
 import UIKit
 
+var imageCache = [String: UIImage]()
+
 class StuffedImageView: UIImageView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    var lastURLUsedToLoadImage: String?
+    
+    func loadImage(urlString: String) {
+        lastURLUsedToLoadImage = urlString
+        
+        self.image = nil
+        
+        if let cachedImage = imageCache[urlString] {
+            self.image = cachedImage
+            return
+        }
+        
+        guard let url = URL(string: urlString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, err) in
+            if let err = err {
+                print("Failed to fetch post image:", err)
+                return
+            }
+            
+            if url.absoluteString != self.lastURLUsedToLoadImage {
+                return
+            }
+            
+            guard let imageData = data else { return }
+            
+            let photoImage = UIImage(data: imageData)
+            
+            imageCache[url.absoluteString] = photoImage
+            
+            DispatchQueue.main.async {
+                self.image = photoImage
+            }
+            
+            }.resume()
     }
-    */
-
+    
 }
