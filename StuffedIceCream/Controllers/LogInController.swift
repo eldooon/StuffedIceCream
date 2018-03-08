71a827e04799c09095c6e6d8a6813eb027693214
eldooon <eldonchan91@gmail.com>
2018-03-08 09:52:16 -0500

    Added logincontroller

---
+++ StuffedIceCream/Controllers/LogInController.swift
@@ -0,0 +1,35 @@
+//
+//  LogInController.swift
+//  StuffedIceCream
+//
+//  Created by Eldon Chan on 3/8/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class LogInController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
