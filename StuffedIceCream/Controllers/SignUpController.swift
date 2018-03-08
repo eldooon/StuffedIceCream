8b9a26de8162267aeac2eb54465a2697366a1ef7
eldooon <eldonchan91@gmail.com>
2018-03-08 13:26:45 -0500

    Added cancel button targer to dismiss VC

--- StuffedIceCream/Controllers/SignUpController.swift
+++ StuffedIceCream/Controllers/SignUpController.swift
@@ -9,11 +9,22 @@
 import UIKit
 
 class SignUpController: UIViewController {
+    
+    let cancelButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Cancel", for: .normal)
+        button.titleLabel?.font = UIFont.primaryBold
+        button.setTitleColor(.stuffedBlue, for: .normal)
+        button.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        createLayout()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +32,14 @@ class SignUpController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @objc func cancelButtonTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+    private func createLayout() {
+        
+        view.addSubview(cancelButton)
+        cancelButton.anchor(centerX: nil, centerY: nil, top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: nil, paddingTop: 30, paddingLeft: 10, paddingBottom: 0, paddingRight: 0, width: 60, height: 20)
+    }
 
     /*
     // MARK: - Navigation
