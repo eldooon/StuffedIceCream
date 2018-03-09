cdf97941b7f0bb9e7acea2988fdbe65b3e3eb9b8
eldooon <eldonchan91@gmail.com>
2018-03-09 12:55:34 -0500

    Overide touchesbegan

--- StuffedIceCream/Views/StuffedButton.swift
+++ StuffedIceCream/Views/StuffedButton.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class StuffedButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        self.transform = CGAffineTransform(scaleX: 1.2, y: 1.2)
+        
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 6, options: .curveEaseInOut, animations: {
+            self.transform = CGAffineTransform.identity
+        }, completion: nil)
     }
-    */
+
 
 }
