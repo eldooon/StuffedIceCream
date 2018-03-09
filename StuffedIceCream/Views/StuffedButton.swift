5b27e1f7f72bf0fef7fec9c483d3b7844c2c4f00
eldooon <eldonchan91@gmail.com>
2018-03-09 12:47:47 -0500

    added StuffedButton file

---
+++ StuffedIceCream/Views/StuffedButton.swift
@@ -0,0 +1,21 @@
+//
+//  StuffedButton.swift
+//  StuffedIceCream
+//
+//  Created by Eldon Chan on 3/9/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class StuffedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
