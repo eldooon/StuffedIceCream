8c6d86af9a3d0acff08397ca58a235e2a0906a6c
eldooon <eldonchan91@gmail.com>
2018-03-18 15:13:10 -0400

    Completed footercell

--- StuffedIceCream/Views/MyAccountFooterCell.swift
+++ StuffedIceCream/Views/MyAccountFooterCell.swift
@@ -10,4 +10,28 @@ import UIKit
 
 class MyAccountFooterCell: UICollectionViewCell {
     
+    let noCouponsLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.primaryBold
+        label.textAlignment = .center
+        label.text = "Aw cruff, looks like you currently do not have any coupons"
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        createLayout()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func createLayout(){
+        
+        addSubview(noCouponsLabel)
+        noCouponsLabel.anchor(centerX: centerXAnchor, centerY: centerYAnchor, top: nil, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 5, paddingBottom: 0, paddingRight: 5, width: 0, height: 50)
+        
+    }
 }
