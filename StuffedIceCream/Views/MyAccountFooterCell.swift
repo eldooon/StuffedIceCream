fdd4702eab085deb0e94c6e901cfeab76808fdb9
eldooon <eldonchan91@gmail.com>
2018-03-18 15:10:37 -0400

    Added MyAccountFooterCell

---
+++ StuffedIceCream/Views/MyAccountFooterCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyAccountFooterCell.swift
+//  StuffedIceCream
+//
+//  Created by Eldon Chan on 3/18/18.
+//  Copyright © 2018 ByEldon. All rights reserved.
+//
+
+import UIKit
+
+class MyAccountFooterCell: UICollectionViewCell {
+    
+}
